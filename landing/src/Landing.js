83f3e96e3132d468b458607497f87052127194d4
Roman Mendaliev <reacheight8@gmail.com>
2024-05-25 21:14:06 +0300

    add color scheme and buttons

--- landing/src/Landing.js
+++ landing/src/Landing.js
@@ -2,20 +2,31 @@ import './Landing.css'
 import { TypeAnimation } from 'react-type-animation'
 
 const Landing = () => {
+  const ctaButton = (
+    <div className='ctaButton'>
+      Оставить заявку
+    </div>
+  )
+
   return (
     <div className='container'>
       <div className='header'>
         <div className='logo'>kodifix</div>
+        {ctaButton}
       </div>
       <div className='description'>
-        Становись{' '}
-        <span className='purpleSpan'>
+        Стань{' '}
+        <span className='highlight'>
           <TypeAnimation
             sequence={['магом', 2000, 'рыцарем', 2000, 'шпионом', 2000, 'ведьмой', 2000, 'ниндзя', 2000]}
             repeat={Infinity}
           />
         </span>
-        и изучай программирование на Python
+        и изучи
+        <div>программирование на Python.</div>
+      </div>
+      <div className='buttonUnderDescription'>
+        {ctaButton}
       </div>
     </div>
   )
