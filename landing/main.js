1edc826e006e858af512b32a6ec649ddf9672d5b
Roman Mendaliev <reacheight8@gmail.com>
2024-06-20 23:04:28 +0300

    again

--- landing/main.js
+++ landing/main.js
@@ -3,7 +3,15 @@ const statApp = () => {
         document.querySelector('.application-form').scrollIntoView()
     })
 
-    new Typewriter('#typewriter-effect', {
+    new Typewriter('#typewriter-effect-desktop', {
+        strings: ['волшебником', 'рыцарем', 'шпионом', 'ниндзя'],
+        delay: 50,
+        deleteSpeed: 50,
+        autoStart: true,
+        loop: true,
+    });
+
+    new Typewriter('#typewriter-effect-mobile', {
         strings: ['волшебником', 'рыцарем', 'шпионом', 'ниндзя'],
         delay: 50,
         deleteSpeed: 50,
