8ecf1c006a692d32f0561c1f5495e5df607ff78a
Roman Mendaliev <reacheight8@gmail.com>
2024-07-05 21:03:43 +0300

    add wall types

--- backend/levels.js
+++ backend/levels.js
@@ -28,8 +28,8 @@ export const levels = {
     hero: { x: 0, y: 0 },
     finish: { x: 2, y: 4 },
     walls: [
-      { x: 0, y: 1 }, { x: 1, y: 1 },
-      { x: 2, y: 3 },
+      { x: 0, y: 1, type: 'rock' }, { x: 1, y: 1, type: 'tree' },
+      { x: 2, y: 3, type: 'rock' },
     ],
     gems: [ { x: 0, y: 4 }],
     enemies: [],
@@ -46,8 +46,8 @@ export const levels = {
       { x: 8, y: 3 }
     ],
     walls: [
-      { x: 0, y: 3 }, { x: 1, y: 3 }, { x: 2, y: 3 }, { x: 3, y: 3 }, { x: 4, y: 3 }, { x: 5, y: 3 }, { x: 6, y: 3 }, { x: 7, y: 3 },
-      { x: 7, y: 4 }, { x: 8, y: 4 }
+      { x: 0, y: 3, type: 'rock' }, { x: 1, y: 3, type: 'rock' }, { x: 2, y: 3, type: 'rock' }, { x: 3, y: 3, type: 'rock' }, { x: 4, y: 3, type: 'rock' }, { x: 5, y: 3, type: 'rock' }, { x: 6, y: 3, type: 'rock' }, { x: 7, y: 3, type: 'rock' },
+      { x: 7, y: 4, type: 'tree' }, { x: 8, y: 4, type: 'tree' }
     ],
     enemies: [ { x: 9, y: 5, alive: true, name: "John" }]
   },
