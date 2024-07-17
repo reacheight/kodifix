e885356b1538d2db8e2ddba08b4a36df4feabc2b
Roman Mendaliev <reacheight8@gmail.com>
2024-07-17 20:36:19 +0300

    поправил детект наших ошибок, хз сильно ли замедлит

--- backend/CodeAnalyzer.js
+++ backend/CodeAnalyzer.js
@@ -1,35 +1,30 @@
 export default class CodeAnalyzer {
   analyze(rawCode) {
-    const noParenthesisMatch = rawCode.match(/^ *(hero\.[^\d\W]\w*)$/mi)
-    if (noParenthesisMatch) {
-      return [
-        {
-          message: `Чтобы вызвать \`${noParenthesisMatch[0]}\`, нужно добавить \`()\`.`,
-          line: this.calculateLine(rawCode, noParenthesisMatch.index),
-        }
-      ]
-    }
+    const lines = rawCode.split(/\r\n|\r|\n/);
+    for (let i = 0; i < lines.length; i++) {
+      const lineCode = lines[i];
 
-    const dashMatch = rawCode.match(/(hero.move-down)|(hero.move-up)|(hero.move-left)|(hero.move-right)|(find-nearest-enemy)/)
-    if (dashMatch) {
-      return [
-        {
-          message: `В методе \`${dashMatch[0]}\` должно быть \`_\` вместо \`-\`.`,
-          line: this.calculateLine(rawCode, dashMatch.index),
-        }
-      ]
+      const noParenthesisMatch = lineCode.match(/^ *(hero\.[^\d\W]\w*)$/i);
+      if (noParenthesisMatch) {
+        return [
+          {
+            message: `Чтобы вызвать \`${noParenthesisMatch[0]}\`, нужно добавить \`()\`.`,
+            line: i + 1,
+          }
+        ];
+      }
+  
+      const dashMatch = lineCode.match(/(hero.move-down)|(hero.move-up)|(hero.move-left)|(hero.move-right)|(find-nearest-enemy)/)
+      if (dashMatch) {
+        return [
+          {
+            message: `В методе \`${dashMatch[0]}\` должно быть \`_\` вместо \`-\`.`,
+            line: i + 1,
+          }
+        ];
+      }
     }
 
     return [];
-  }
-
-  calculateLine(rawCode, matchIndex) {
-    const lines = rawCode.split(/\r\n|\r|\n/);
-    let indexCounter = 0;
-    for (let i = 0; i < lines.length; i++) {
-      indexCounter += lines[i].length;
-      if (indexCounter > matchIndex)
-        return i + 1;
-    }
   }
 }
