24003cd27a84355d51a7b97ccbdd6f5b79a093c4
Roman Mendaliev <reacheight8@gmail.com>
2024-07-19 19:48:24 +0300

    добавил разрешенных хостов для корсов

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -1,5 +1,5 @@
 export const cors = (_req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://kodifix.ru, https://app.kodifix.ru, https://demo.kodifix.ru');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
