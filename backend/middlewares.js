7c73493fe753bbfda7b341bcbdad70c5a85ea8d8
Roman Mendaliev <reacheight8@gmail.com>
2024-07-20 21:58:57 +0300

    поправил логику нескольких допустимых хостов для корсов

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -1,5 +1,9 @@
-export const cors = (_req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://kodifix.ru, https://app.kodifix.ru, https://demo.kodifix.ru');
+export const cors = (req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://kodifix.ru', 'https://app.kodifix.ru', 'https://demo.kodifix.ru']
+  const origin = req.header('origin').toLowerCase();
+  if (allowedOrigins.includes(origin))
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
