abb4c68cb38adda7b49e55ca8cd627763ff0dabd
Roman Mendaliev <reacheight8@gmail.com>
2024-08-24 15:23:51 +0300

    маплю ошибку про пустой иф

--- backend/ErrorMessageMapper.js
+++ backend/ErrorMessageMapper.js
@@ -3,6 +3,9 @@ export default class ErrorMessageMapper {
         if (esperMessage.startsWith("Unmatched `(`"))
             return "Нет закрывающей скобки `)`.";
 
+        if (esperMessage === "Empty if statement. Put 4 spaces in front of statements inside the if statement.")
+            return "Пустая конструкция if. Ты забыл 4 пробела перед выражением?"
+
         const noOpeningParantethisMatch = esperMessage.match(/If you want to call (`.*`) as function, you need `\(\)`'s/);
         if (noOpeningParantethisMatch) {
             return `Если ты хочешь вызвать метод ${noOpeningParantethisMatch[1]}, нужно добавить \`()\`.`;
