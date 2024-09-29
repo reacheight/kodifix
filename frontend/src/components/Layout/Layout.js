85a499fadc6ba488c9cb2b44c1c3e5e977784072
Roman Mendaliev <reacheight8@gmail.com>
2024-09-29 20:54:05 +0300

    просто рефрешу страницу при логине, чтобы всё обновилось

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -26,6 +26,7 @@ export const Layout = ({ children }) => {
       .then(data => {
         Cookies.set('yaToken', data.access_token, { path: '/', domain: '.kodifix.ru', secure: true, sameSite: 'Lax', expires: Math.floor(Number(data.expires_in) / 86400) })
         setAuthToken(data.access_token);
+        location.reload();
       })
       .catch(error => console.log('Обработка ошибки', error));
     }
