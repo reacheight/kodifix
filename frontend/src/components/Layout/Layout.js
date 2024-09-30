335d6500e5869bb145c6559311c17165f55d5718
Roman Mendaliev <reacheight8@gmail.com>
2024-09-30 21:49:24 +0300

    пробую кнопку вместо виджета

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -10,22 +10,45 @@ const LayoutWrapper = styled.div`
 
 export const Layout = ({ children }) => {
   useEffect(() => {
+    const params = {
+      client_id: '064dd0d0ea2d41bd8867928cd9704763',
+      response_type: 'token',
+      redirect_uri: 'https://demo.kodifix.ru/ya_callback'
+    };
+
     if (!document.cookie.includes('yaToken') && window.YaAuthSuggest) {
+      // window.YaAuthSuggest.init(
+      //   params,
+      //   'https://demo.kodifix.ru',
+      //   { view: "default" }
+      // )
+      // .then(({handler}) => handler())
+      // .then(data => {
+      //   Cookies.set('yaToken', data.access_token, { path: '/', domain: '.kodifix.ru', secure: true, sameSite: 'Lax', expires: Math.floor(Number(data.expires_in) / 86400) })
+      //   location.reload();
+      // })
+      // .catch(error => console.log('Обработка ошибки', error));
+
       window.YaAuthSuggest.init(
-        {
-          client_id: '064dd0d0ea2d41bd8867928cd9704763',
-          response_type: 'token',
-          redirect_uri: 'https://demo.kodifix.ru/ya_callback'
-        },
+        params,
         'https://demo.kodifix.ru',
-        { view: "default" }
+        {
+          view: "button",
+          parentId: "buttonContainerId",
+          buttonSize: 'm',
+          buttonView: 'main',
+          buttonTheme: 'light',
+          buttonBorderRadius: "10",
+          buttonIcon: 'ya',
+        }
       )
       .then(({handler}) => handler())
       .then(data => {
         Cookies.set('yaToken', data.access_token, { path: '/', domain: '.kodifix.ru', secure: true, sameSite: 'Lax', expires: Math.floor(Number(data.expires_in) / 86400) })
         location.reload();
       })
-      .catch(error => console.log('Обработка ошибки', error));
+      .catch(error => console.log('Обработка ошибки', error))
+    
     }
   }, []);
 
