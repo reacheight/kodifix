a4410c3f159419c8fbc7c24837140a1927dcedca
Roman Mendaliev <reacheight8@gmail.com>
2024-11-16 19:11:07 +0300

    фикшу запрос на создание юзера

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -46,7 +46,7 @@ export const Layout = ({ children }) => {
       .then(({handler}) => handler())
       .then(data => {
         Cookies.set('yaToken', data.access_token, { path: '/', domain: '.kodifix.ru', secure: true, sameSite: 'Lax', expires: Math.floor(Number(data.expires_in) / 86400) })
-        axios.post(`/user`, { withCredentials: true });
+        axios.post(`/user`, {}, { withCredentials: true });
         location.reload();
       })
       .catch(error => console.log('Обработка ошибки', error))
