972f2c06316f423839e4087060c347f75ee9c218
Roman Mendaliev <reacheight8@gmail.com>
2025-06-15 17:42:47 +0300

    update module preview image and reduce top padding

--- frontend/src/components/MainPage/styled.js
+++ frontend/src/components/MainPage/styled.js
@@ -15,7 +15,7 @@ const currentLevelAnimation = keyframes`
 
 export const Container = styled.div`
   padding: 20px;
-  padding-top: 200px;
+  padding-top: 150px;
   min-height: 100vh;
   background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
   display: flex;
